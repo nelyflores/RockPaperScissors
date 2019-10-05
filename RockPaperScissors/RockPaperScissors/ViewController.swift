060f7c0a9fa41df56825731c30a5ee958a9e45f7
Ana Salazar <anastephany.salazarguzman@gmail.com>
2019-10-05 12:31:42 -0500

    SetUp(): Add project

---
+++ RockPaperScissors/RockPaperScissors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RockPaperScissors
+//
+//  Created by Ana Steph on 10/5/19.
+//  Copyright © 2019 Ana Steph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
